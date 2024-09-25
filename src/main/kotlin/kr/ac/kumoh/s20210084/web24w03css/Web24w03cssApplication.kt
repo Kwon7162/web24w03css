0040e9189cadfba320bbb75ef9c2fe7d02faa689
권찬호 <rnjscksdud17@gmail.com>
2024-09-25 13:38:03 +0900

    start

---
+++ src/main/kotlin/kr/ac/kumoh/s20210084/web24w03css/Web24w03cssApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210084.web24w03css
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Web24w03cssApplication
+
+fun main(args: Array<String>) {
+	runApplication<Web24w03cssApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20210084/web24w03css/Web24w03cssApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210084.web24w03css
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Web24w03cssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
